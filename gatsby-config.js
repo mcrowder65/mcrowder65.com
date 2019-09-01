8110c3d21dd17b2cae35c0b5f24ca8233969ad65
Matt Crowder <mcrowder65@gmail.com>
2019-09-01 10:20:40 -0400

    added google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -57,7 +57,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: "UA-146848164-1",
       },
     },
     `gatsby-plugin-feed`,
